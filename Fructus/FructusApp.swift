b5548f0f38c51f4dd974938c1a115740bf370845
marcoalonso <marcoalonsorota@hotmail.com>
2021-06-24 11:09:53 -0500

    Getting-Started

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by marco rodriguez on 24/06/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
